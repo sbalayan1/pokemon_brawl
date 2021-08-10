f5d2f757b95772b53aad15a1edf9de4e39559d10
sbalayan1 <balayans2014@yahoo.com>
2021-08-10 07:32:08 -0700

    created user model, controller, serializer, and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,21 @@
 User.delete_all
 
 puts 'create user'
+
 User.create(username: 'sean', first_name: 'sean', last_name: 'balayan', email_address: 'balayans2014@yahoo.com', age: 25, password: '123456', password_confirmation: '123456')
+
+
+
+10.times do 
+    password = Faker::Alphanumeric.alpha(number: 10)
+    age = rand(5..50)
+
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    username = first_name + last_name
+
+    User.create(username: username, first_name: first_name,last_name: last_name, password: password, password_confirmation: password, email_address: Faker::Internet.email, age: age)
+end
+
+
+puts 'done seeding'
