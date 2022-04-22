9cd562fb9860fa6986e2533f64487a7db6111853
sbalayan1 <balayans2014@yahoo.com>
2022-04-21 17:55:29 -0700

    remove nes.react

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 import { Switch, Route, useHistory } from 'react-router-dom'
 import * as React from 'react'
-import {Container} from 'nes-react'
+// import {Container} from 'nes-react'
 
 //import react components
 import NavBar from './NavBar'
