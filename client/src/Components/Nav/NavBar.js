51ffca96adcb403efefbd177a124248552eb9874
sbalayan1 <balayans2014@yahoo.com>
2022-06-17 01:30:06 -0700

    update NavBar

--- client/src/Components/Nav/NavBar.js
+++ client/src/Components/Nav/NavBar.js
@@ -3,16 +3,23 @@ import {NavLink} from 'react-router-dom'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import {useMediaQuery} from 'react-responsive'
 
 let NavBar = ({pokeBall, currentUser}) => {
-    let notLoggedInPaths = ['Login', 'Sign Up', 'Logout']
-    let loggedInPaths = ['Home', 'Safari Zone', 'Battle', 'My PC', 'Leaderboards', 'Logout']
+    let isMobile = useMediaQuery({ query: '(max-width: 980px)' })
+    console.log(isMobile)
+
+    let notLoggedInPaths = isMobile ? ['Login', 'Logout'] : ['Login', 'Sign Up', 'Logout']
+    let loggedInPaths = isMobile ? ['Battle', 'PC', 'Logout'] : ['Home', 'Safari Zone', 'Battle', 'My PC', 'Leaderboards', 'Logout']
+
     let notLoggedIn = notLoggedInPaths.map(path => (
         <Link key={path} path={path}/>
     ))
     let loggedIn = loggedInPaths.map(path => (
         <Link key={path} path={path}/>
     ))
+
+    let letterSpacing = isMobile ? '.2rem' : '.3rem'
 
     let renderNavBar = () => {
         return (
@@ -25,17 +32,17 @@ let NavBar = ({pokeBall, currentUser}) => {
                             component="a"
                             href="/"
                             sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
+                                mr: 2,
+                                display: { xs: 'flex', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: {letterSpacing},
+                                color: 'inherit',
+                                textDecoration: 'none',
                             }}>
                                 <NavLink to="/" className="logo" >
                                     {pokeBall ? <img src={pokeBall} alt='pokeball'/> : null}
-                                    PokemonBrawl
+                                    {isMobile ? 'PokeBrawl' : 'PokemonBrawl'}
                                 </NavLink>
                         </Typography>
                         {currentUser ? loggedIn : notLoggedIn}
